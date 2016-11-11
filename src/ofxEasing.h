efbb3eb03506ce06fdbe516084fab5e1c89342e2
arturo castro <arturo@openframeworks.cc>
2016-11-11 11:34:27 -0300

    ofxEasing: add power parameter to elastic

--- src/ofxEasing.h
+++ src/ofxEasing.h
@@ -101,34 +101,34 @@ class cubic{
 
 class elastic{
 	public:
-	inline static float easeIn (float t,float b , float c, float d) {
+	inline static float easeIn (float t,float b , float c, float d, float power=10) {
 		if (t==0) return b;  if ((t/=d)==1) return b+c;
 		float p=d*.3f;
 		float a=c;
 		float s=p/4;
-		float postFix =a*pow(2,10*(t-=1)); // this is a fix, again, with post-increment operators
+		float postFix =a*pow(2,power*(t-=1)); // this is a fix, again, with post-increment operators
 		return -(postFix * sin((t*d-s)*(2*float(pi))/p )) + b;
 	}
 
-	inline static float easeOut(float t,float b , float c, float d) {
+	inline static float easeOut(float t,float b , float c, float d, float power=10) {
 		if (t==0) return b;  if ((t/=d)==1) return b+c;
 		float p=d*.3f;
 		float a=c;
 		float s=p/4;
-		return (a*pow(2,-10*t) * sin( (t*d-s)*(2*pi)/p ) + c + b);
+		return (a*pow(2,-power*t) * sin( (t*d-s)*(2*pi)/p ) + c + b);
 	}
 
-	inline static float easeInOut(float t,float b , float c, float d) {
+	inline static float easeInOut(float t,float b , float c, float d, float power=10) {
 		if (t==0) return b;  if ((t/=d/2)==2) return b+c;
 		float p=d*(.3f*1.5f);
 		float a=c;
 		float s=p/4;
 
 		if (t < 1) {
-			float postFix =a*pow(2,10*(t-=1)); // postIncrement is evil
+			float postFix =a*pow(2,power*(t-=1)); // postIncrement is evil
 			return -.5f*(postFix* sin( (t*d-s)*(2*pi)/p )) + b;
 		}
-		float postFix =  a*pow(2,-10*(t-=1)); // postIncrement is evil
+		float postFix =  a*pow(2,-power*(t-=1)); // postIncrement is evil
 		return postFix * sin( (t*d-s)*(2*pi)/p )*.5f + c + b;
 	}
 };
